95db62a3bc90f7752a1e56ec7924edef059ff946
SeveriusRasmussen <sora0001@stud.kea.dk>
2024-03-18 14:47:09 +0100

    First commit: Attributes and constructor, get/set, .txt file added.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("-----Welcome to the student phonenumber list-----");
+        System.out.println("""
+                           1. for new entry
+                           2. for show list
+                           3. for delete name/number
+                           4. to quit
+                           """);
+
+        Scanner input = new Scanner(System.in);
+
+        // Switch Case for the input
+
+    }
+}
---
+++ src/main/java/org/example/StudentInfo.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class StudentInfo {
+    private String fullName;
+    private int phoneNr;
+
+    //private ArrayList<>
+
+    public StudentInfo(String fullName, int phoneNr) {
+        this.fullName = fullName;
+        this.phoneNr = phoneNr;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getPhoneNr() {
+        return phoneNr;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPhoneNr(int phoneNr) {
+        this.phoneNr = phoneNr;
+    }
+
+    // Create method
+    //
+}
